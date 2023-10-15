bd43d8bf9dc3083673c3fdc449eb4b751187e633
JuankoHF <108952217+JuankoHF@users.noreply.github.com>
2023-10-15 10:31:42 +0200

    secction lista hasta tablet

---
+++ proJS/ref_email.js
@@ -0,0 +1,11 @@
+// Obtén una referencia al botón por su ID
+const emailButton = document.getElementById('emailButton');
+
+// Agrega un manejador de evento clic al botón
+emailButton.addEventListener('click', function() {
+    // Definimos la dirección de correo electrónico a la que se enviará el correo
+    const emailAddress = 'laura080421@gmail.com';
+    
+    // Abre el cliente de correo electrónico del usuario con un nuevo correo
+    window.location.href = 'mailto:' + emailAddress;
+});
