e4dbdc36a61537d199d65a7b3185ed2cdc505d39
JuankoHF <108952217+JuankoHF@users.noreply.github.com>
2023-10-26 17:03:12 +0200

    pages lista para produccion

---
+++ proJS/animateIndex.js
@@ -0,0 +1,55 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.header_menu', {
+        duration: 2500,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    // sr.reveal('.nav_menu', {
+    //     duration: 2500,
+    //     origin: 'bottom',
+    //     distance: '-100px'
+    // });
+
+    sr.reveal('.contente_description', {
+        duration: 2500,
+        origin: 'left',
+        distance: '200px'
+    });
+
+    sr.reveal('.description_reveal_h1', {
+        duration: 2500,
+        origin: 'right',
+        distance: '200px'
+    });
+
+    sr.reveal('.description_reveal_p1', {
+        duration: 3000,
+        origin: 'right',
+        distance: '200px'
+    });
+
+    sr.reveal('.description_reveal_p2', {
+        duration: 3500,
+        origin: 'right',
+        distance: '200px'
+    });
+
+    sr.reveal('.description_reveal_p3', {
+        duration: 4000,
+        origin: 'right',
+        distance: '200px'
+    });
+
+    sr.reveal('.loader', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '200px'
+    });
+
+    sr.reveal('.text_action_content', {
+        duration: 3500,
+        origin: 'bottom',
+        distance: '200px'
+    });
