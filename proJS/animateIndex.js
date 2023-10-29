8e343a6f05c36eb2eead024d94e3afe1f5e8d8d3
JuankoHF <108952217+JuankoHF@users.noreply.github.com>
2023-10-29 12:35:51 +0100

    loader listo video arranca bien

--- proJS/animateIndex.js
+++ proJS/animateIndex.js
@@ -1,9 +1,10 @@
 window.sr = ScrollReveal();
 
-    sr.reveal('.header_menu', {
+    sr.reveal('header', {
         duration: 2500,
         origin: 'bottom',
-        distance: '-100px'
+        distance: '-100px',
+        delay: 3000
     });
 
     // sr.reveal('.nav_menu', {
@@ -15,41 +16,48 @@ window.sr = ScrollReveal();
     sr.reveal('.contente_description', {
         duration: 2500,
         origin: 'left',
-        distance: '200px'
+        distance: '200px',
+        delay: 3000
     });
 
     sr.reveal('.description_reveal_h1', {
         duration: 2500,
         origin: 'right',
-        distance: '200px'
+        distance: '200px',
+        delay: 3000
     });
 
     sr.reveal('.description_reveal_p1', {
         duration: 3000,
         origin: 'right',
-        distance: '200px'
+        distance: '200px',
+        delay: 3000
     });
 
     sr.reveal('.description_reveal_p2', {
         duration: 3500,
         origin: 'right',
-        distance: '200px'
+        distance: '200px',
+        delay: 3000
     });
 
     sr.reveal('.description_reveal_p3', {
         duration: 4000,
         origin: 'right',
-        distance: '200px'
+        distance: '200px',
+        delay: 3000
     });
 
     sr.reveal('.loader', {
-        duration: 3000,
+        duration: 3500,
         origin: 'bottom',
-        distance: '200px'
+        distance: '200px',
+        delay: 1500
     });
 
     sr.reveal('.text_action_content', {
         duration: 3500,
         origin: 'bottom',
-        distance: '200px'
+        distance: '200px',
+        delay: 2000
     });
